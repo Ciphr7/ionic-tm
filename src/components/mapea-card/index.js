950ba9c6dd588996a8b8c12c64db8b2d102becbd
Ciphr7 <gil@ciphr7.com>
2022-06-04 15:32:11 -0500

    changes to menu

--- src/components/mapea-card/index.js
+++ src/components/mapea-card/index.js
@@ -8,36 +8,25 @@ export default class MapeaCard extends HTMLElement {
         this.olMap.updateSize()
       }
     })
-    //TODO: throw error if not mapid
-    this._mapid = this.getAttribute('mapid')
-    this._center = JSON.parse(this.getAttribute('center'))
-    this._zoom = this.getAttribute('zoom')
-    this._title = this.getAttribute('title')
-    this._category = this.getAttribute('category')
-    this._description = this.getAttribute('description')
-  }
-  connectedCallback() {
-    this.innerHTML = html
-    this._divMap = this.querySelector('.map-card')
-    this._divMap.id = this._mapid
-    this._setInfo(this._title, this._category, this._description)
-    this._createMap(this._divMap, this._center, this._zoom)
-  }
+  }}
+  import { GoogleMap } from '@capacitor/google-maps';
 
-  _createMap(div, center, zoom) {
-    let mMap = M.map({
-      container: div,
-      center: center,
-      zoom: zoom,
-    })
-    this.olMap = mMap.getMapImpl()
-    this.sizeObserver.observe(div)
-  }
+  const createMap = async () => {
+    const mapRef = document.getElementById('map');
 
-  //TODO: add interpolation (i.e. Handlebars)
-  _setInfo(title, subtitle, description) {
-    this.querySelector('ion-card-title').innerHTML = title
-    this.querySelector('ion-card-subtitle').innerHTML = subtitle
-    this.querySelector('ion-card-content').innerHTML = description
-  }
-}
+    const newMap = await GoogleMap.create({
+      id: 'my-map', // Unique identifier for this map instance
+      element: mapRef, // reference to the capacitor-google-map element
+      apiKey: 'YOUR_API_KEY_HERE', // Your Google Maps API Key
+      config: {
+        center: {
+          // The initial position to be rendered by the map
+          lat: 33.6,
+          lng: -117.9,
+        },
+        zoom: 8, // The initial zoom level to be rendered by the map
+      },
+    });
+  };
+  
+
